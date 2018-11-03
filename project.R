eef18d9bd75eb0a815b7e4c8b181bfef94c3e40c
建颖 梁 <UnaLiang@jianyings-MacBook-Pro.local>
2018-11-03 12:46:43 -0400

    Jianying Liang

--- project.R
+++ project.R
@@ -1,10 +1,30 @@
 library(simts)
-install.packages("pageviews")
 library(pageviews)
-wiki_ts = project_pageviews(granularity = "daily", start = "2017090123", end = "2018102123")
-tail(wiki_ts)
-Xt = gts(wiki_ts$views)
-plot(Xt)
-mod = estimate(SARIMA(ar = 2, i = 0, ma = 1, sar = 2, si = 0, sma = 1, s = 7), Xt,
-               method = "rgmwm")
-check(mod)
+
+wiki_mobile = project_pageviews(granularity = "daily", start = "2017090123", end = "2018102123",platform = "mobile-app")
+Xt_mobile=gts(wiki_mobile$views)
+plot(Xt_mobile)
+
+wiki_desk = project_pageviews(granularity = "daily", start = "2017090123", end = "2018102123",platform = "desktop")
+Xt_desk=gts(wiki_desk$views)
+plot(Xt_desk)
+
+wiki_Sil = article_pageviews(article = 'Silvio_Berlusconi', start = "2018090100", end = "2018102100")
+Xt_Sil=gts(wiki_Sil$views)
+plot(Xt_Sil)
+
+wiki_Bey = article_pageviews(article = 'Beyonce', start = "2018090100", end = "2018102100")
+Xt_Bey=gts(wiki_Bey$views)
+plot(Xt_Bey)
+
+wiki_Noam = article_pageviews(article = 'Noam_Chomsky', start = "2018090100", end = "2018102100")
+Xt_Noam=gts(wiki_Noam$views)
+plot(Xt_Noam)
+
+wiki_lazio = article_pageviews(article = 'SS_Lazio', start = "2018090100", end = "2018102100")
+Xt_lazio=gts(wiki_lazio$views)
+plot(Xt_lazio)
+
+wiki_Thanks = article_pageviews(article = 'Thanksgiving', start = "2018090100", end = "2018102100")
+Xt_Thanks=gts(wiki_Thanks$views)
+plot(Xt_Thanks)
