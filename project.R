c5547b686d9f5465f4d1889795385888aae010cf
jiajun <jxg449@psu.edu>
2018-11-05 01:01:27 -0500

    Jiajun

--- project.R
+++ project.R
@@ -18,6 +18,7 @@ wiki_Bey = article_pageviews(article = 'Beyonce', start = "2018090100", end = "2
 Xt_Bey=gts(wiki_Bey$views)
 plot(Xt_Bey)
 
+plot(auto_corr(Xt_mobile))
 wiki_Noam = article_pageviews(article = 'Noam_Chomsky', start = "2018090100", end = "2018102100")
 Xt_Noam=gts(wiki_Noam$views)
 plot(Xt_Noam)
@@ -29,3 +30,9 @@ plot(Xt_lazio)
 wiki_Thanks = article_pageviews(article = 'Thanksgiving', start = "2018090100", end = "2018102100")
 Xt_Thanks=gts(wiki_Thanks$views)
 plot(Xt_Thanks)
+
+
+
+plot(auto_corr(Xt_mobile))
+mod = estimate(SARIMA(ar = 1, i = 0, ma = 0, sar = 0, si = 0, sma = 0, s = 4), Xt_mobile)
+check(mod)
